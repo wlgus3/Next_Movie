6f77a4e01a33dc287710183f89b3fa43e659bfb8
Jihyun <jhjh3903@gmail.com>
2024-05-11 23:59:28 +0900

    feat: 디테일 작업중

--- src/app/community/movie/_component/MovieDetail.tsx
+++ src/app/community/movie/_component/MovieDetail.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "../../../../../node_modules/next/navigation";
 import { getMovieDetail, getDetailMovie } from "@/api/index";
 import Image from "next/image";
@@ -8,12 +8,27 @@ export default function MovieDetail() {
   const params = useParams();
 
   const movieId = params.id;
-  const data = useQuery({ querKey: ["movieDetail", +movieId], queryFn: movieId ? () => getDetailMovie(+movieId) : undefined });
-  const detailData=data.data
-  console.log(data.data);
+  const response = useQuery({ querKey: ["movieDetail", +movieId], queryFn: movieId ? () => getDetailMovie(+movieId) : undefined });
+  const data = response.data;
+  console.log(response.data);
   return (
     <div>
-      <Image alt="movie image" src={`https://image.tmdb.org/t/p/w500${detailData.poster_path}`} height={700} width={500} unoptimized={true} />
+      <div className="detail-brief">
+        <div className="text-lg font-bold">{data.title}</div>
+        <div>
+          {data.genres.map((el) => {
+            return <span> {el.name} /</span>;
+          })}
+          <span>{data.release_date}</span>
+        </div>
+        <Image alt="movie image" src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} height={280} width={200} unoptimized={true} />
+
+      </div>
+      <div className="detail_description">
+        <div>
+          <span>개요</span><span>{data.overview}</span>
+        </div>
+      </div>
     </div>
   );
 }
